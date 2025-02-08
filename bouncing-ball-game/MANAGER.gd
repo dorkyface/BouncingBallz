50efb048c003bbd0e3e62f07bb6c5b8721339465
dorkyface <69087850+dorkyface@users.noreply.github.com>
2025-02-08 21:28:12 +0200

    Toon Ball Shader
    Replaced the default ball (the basketball) with a toon shaded sphere that I think matches the aesthetic better. The shader is custom with an outline, and was fun to program.

--- bouncing-ball-game/MANAGER.gd
+++ bouncing-ball-game/MANAGER.gd
@@ -7,9 +7,13 @@ var GO : bool = false
 var SCORE : int = 0
 var PREVIOUS_SCORE : int = 0
 var STREAK : int = 0
+var ppref = PlayerPref.new()
+var basketball_data_string = "res://Ball Data Resources/basketball.tres"
+var toonball_data_string = "res://Ball Data Resources/Cartoon.tres"
+
 
 var BALL = "BASKETBALL"
-@onready var BALL_TYPE : BallData = load("res://Ball Data Resources/basketball.tres")
+@onready var BALL_TYPE : BallData = load(toonball_data_string)
 
 func score_point():
 	STREAK += 1
@@ -25,3 +29,4 @@ func on_player_death():
 #TODO Make this function actually save values to system.
 func save():
 	SCORE = STREAK + PREVIOUS_SCORE
+	ppref.set_pref("Score", SCORE)
